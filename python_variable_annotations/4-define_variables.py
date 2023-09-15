15f129787a521602c86a3138ff2befefe02a3562
azizios011 <hdriaziz699@gmail.com>
2023-09-15 15:18:36 +0100

    Define variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -2,12 +2,18 @@
 """indicates that the variable a is of type integer
 (int) and is assigned the value 1."""
 a: int = 1
+
 """indicates that the variable pi is of type float
 float) and is assigned the value 3.14."""
+
 pi: float = 3.14
+
 """ indicates that the variable i_understand_annotations is of
 type boolean (bool) and is assigned the value True."""
+
 i_understand_annotations: bool = True
+
 """indicates that the variable school is of type string
 (str) and is assigned the value "Holberton"."""
+
 school: str = "Holberton"
