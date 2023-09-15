3399babe9304a06155035dacc854303a30259412
azizios011 <hdriaziz699@gmail.com>
2023-09-15 15:31:25 +0100

    Complex types - list of floats

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+
+
 from typing import List
 
+
+""" 'input_list: List[float]' indicates that the function takes
+a parameter 'input_list' that is expected to be a list of floats."""
 def sum_list(input_list: List[float]) -> float:
+    """ '-> float' indicates that the function is expected to return a float."""
     return sum(input_list)
