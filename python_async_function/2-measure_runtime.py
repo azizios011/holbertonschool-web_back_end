151aeb9cbc3d4c053dee6006b87699e9b81ce762
azizios011 <hdriaziz699@gmail.com>
2023-09-18 22:57:42 +0100

    Measure the runtime

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import time
+import asyncio
+from typing import List
+from random import uniform
+
+
+async def wait_random(max_delay: int) -> float:
+    return uniform(0, max_delay)
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
+
+
+def measure_time(n, max_delay):
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time / n
