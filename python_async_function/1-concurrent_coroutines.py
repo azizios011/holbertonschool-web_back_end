ad96c81a31750d45ff106eeb45357e7070c65847
azizios011 <hdriaziz699@gmail.com>
2023-09-18 20:50:34 +0100

    The basics of async

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+""" user request """
+
 
 import asyncio
 from typing import List
 from random import uniform
 
 
+""" an asynchronous coroutine 'wait_random' """
+
+
 async def wait_random(max_delay: int) -> float:
     return uniform(0, max_delay)
+
+
+"""n async routine called wait_n"""
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
