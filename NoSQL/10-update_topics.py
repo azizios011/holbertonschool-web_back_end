184b2a5b6879648100551059d3e652d3cf2156b5
azizios011 <hdriaziz699@gmail.com>
2023-09-25 13:28:13 +0100

    Change school topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,11 +5,7 @@ a school document based on the name"""
 
 def update_topics(mongo_collection, name, topics):
     """Update the topics for the school with the specified name"""
-    result = mongo_collection.update_one({"name": name},
-                                         {"$set": {"topics": topics}})
-
-    """Check if the update was successful"""
-    if result.matched_count == 1:
-        print(f"Topics updated for school {name}")
-    else:
-        print(f"School {name} not found")
+    filtro = {"name": name}
+    remplazo = {
+            "$set": {"topics": topics}}
+    actualizacion = mongo_collection.update_many(filtro, remplazo)
