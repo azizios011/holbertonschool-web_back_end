9133ca1a58c82d3fb437f83a0d6a716967cada7b
azizios011 <hdriaziz699@gmail.com>
2023-09-25 13:50:15 +0100

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python3
-"""a Python script that provides some stats about
-Nginx logs stored in MongoDB"""
-
-
+"""  Python script that provides some statistics """
 from pymongo import MongoClient
 
 
-def log_stats():
-    """Connect to the MongoDB server"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
+def estadistica(mongo_collection, tmp=None):
+    """ statistics on Nginx logs stored in MongoDB """
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    items = {}
+    if tmp:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": tmp}})
+        print(f"\tmethod {tmp}: {value}")
+        return
 
-    """Count the total number of documents in the collection"""
-    total_logs = collection.count_documents({})
-
-    """Count the number of documents with each HTTP method"""
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: collection.count_documents
-                     ({"method": method})for method in methods}
-
-    """Count the number of documents with method=GET and path=/status"""
-    status_check_count = collection.count_documents
-    ({"method": "GET", "path": "/status"})
-
-    """Print the results"""
-    print(f"{total_logs} logs")
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
     print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}:", count)
-    print(f"{status_check_count} status check")
+    for metodo in method:
+        estadistica(client, metodo)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
-    log_stats()
+    client = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    estadistica(client)
