40c7a42b6b3cbc5c2b06508bbd385d0f47e9adaa
azizios011 <hdriaziz699@gmail.com>
2023-09-25 10:19:48 +0100

    Insert a document in Python

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+"""Inserts a new document into a 'MongoDB'
+collection based on keyword arguments."""
+
+
 def insert_school(mongo_collection, **kwargs):
+    """ 'mongo_collection (pymongo.collection.Collection)':
+    The 'MongoDB' collection object. '**kwargs': Keyword
+    arguments representing the fields and values to insert."""
     result = mongo_collection.insert_one(kwargs)
     new_id = result.inserted_id
+    """ 'str': The 'new _id' of the inserted document."""
     return str(new_id)
