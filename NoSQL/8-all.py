80bb965f869807a7dc6f5e7d355f725330283e2f
azizios011 <hdriaziz699@gmail.com>
2023-09-25 10:09:59 +0100

    List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,9 +2,6 @@
 """Lists all documents in a MongoDB collection."""
 
 
-from typing import List
-
-
 def list_all(mongo_collection):
     """ 'mongo_collection (pymongo.collection.Collection)':
     The MongoDB collection object."""
