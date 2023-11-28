cadeedc4bdbcbe8917973fc10d0087e0b1c2c32e
azizios011 <hdriaziz699@gmail.com>
2023-11-28 16:51:48 +0100

    Keep every promise you make and only make promises you can keep

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,11 +1,10 @@
 function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      // Simulate an asynchronous operation (e.g., API call) with setTimeout
-      setTimeout(() => {
-        resolve("Successful response from the API");
-      }, 300); // Simulating a delay of 300 milliseconds
-    });
-  }
-  
-  export default getResponseFromAPI;
-  
+  return new Promise((resolve) => {
+    // Simulate an asynchronous operation (e.g., API call) with setTimeout
+    setTimeout(() => {
+      resolve('Successful response from the API');
+    }, 300); // Simulating a delay of 300 milliseconds
+  });
+}
+
+export default getResponseFromAPI;
