aa8d02509f3fe9baed1cccf03c74c6b8adac2985
azizios011 <hdriaziz699@gmail.com>
2023-12-01 15:01:03 +0100

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,30 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const userPromise = signUpUser(firstName, lastName);
-    const photoPromise = uploadPhoto(fileName);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const signingUser = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
 
-    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+  const uploadingPhoto = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
 
-    const resultArray = [
-      {
-        status: userResult.status,
-        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
-      },
-      {
-        status: photoResult.status,
-        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
-      },
-    ];
-
-    return resultArray;
-  } catch (error) {
-    // Handle any unexpected errors here
-    console.error('An unexpected error occurred:', error);
-    return [];
-  }
+  return [signingUser, uploadingPhoto];
 }
-
-export default handleProfileSignup;
